d4d4c68b6d4de48712c8f7fff0f638524bd7032d
Anne Fausto <a.r.fausto@gmail.com>
2019-10-04 20:56:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -16,6 +18,7 @@ class Shoe
   def brand=(brand)
     @brand = brand
     BRANDS << brand
+    binding.pry
   end 
   
 
